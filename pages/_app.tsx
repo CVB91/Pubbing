7f988f5997a1094418356c447c81f2833bf5b3aa
Christian <cphvb91@gmail.com>
2023-03-04 11:42:16 +0100

    built authprovider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,14 @@ import Head from "next/head";
 import { Fragment } from "react";
 // import { ApolloProvider } from "@apollo/client";
 // import { useApollo } from "src/apollo";
-// import { AuthProvider } from "src/auth/useAuth";
+import { AuthProvider } from "src/auth/useAuth";
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Fragment>
+  return <AuthProvider>
     <Head>
       <title>Pubbing</title>
       <link rel="icon" href="/favicon.ico"/>
     </Head>
-  <Component {...pageProps} /></Fragment>;
+  <Component {...pageProps} /></AuthProvider>;
 }
