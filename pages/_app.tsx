e36f9513ee5cfc2b00fb6f027839a5af28e9794a
Christian <cphvb91@gmail.com>
2023-03-06 22:23:39 +0100

    image upload with cloudinary functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,26 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { Fragment } from "react";
-// import { ApolloProvider } from "@apollo/client";
-// import { useApollo } from "src/apollo";
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "src/apollo";
 import { AuthProvider } from "src/auth/useAuth";
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <AuthProvider>
-    <Head>
-      <title>Pubbing</title>
-      <link rel="icon" href="/favicon.ico"/>
-    </Head>
-  <Component {...pageProps} /></AuthProvider>;
+
+  const client = useApollo()
+
+  return (
+    <AuthProvider>
+      <ApolloProvider client={client}>
+        <Head>
+          <title>Pubbing</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </AuthProvider>
+  );
 }
 
 //Because the Authprovider is wrapped around every page of our application every subcomponent get access to the context
--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -1,7 +1,6 @@
-import { read } from "fs";
 import { useState, useEffect, ChangeEvent } from "react";
 import { useForm } from "react-hook-form";
-// import { useMutation, gql } from "@apollo/client";
+import { useMutation, gql } from "@apollo/client";
 // import { useRouter } from "next/router";
 import Link from "next/link";
 // import { Image } from "cloudinary-react";
@@ -14,7 +13,42 @@ import { SearchBox } from "./searchBox";
 //   UpdateHouseMutation,
 //   UpdateHouseMutationVariables,
 // } from "src/generated/UpdateHouseMutation";
-// import { CreateSignatureMutation } from "src/generated/CreateSignatureMutation";
+import { CreateSignatureMutation } from "src/generated/CreateSignatureMutation";
+
+const SIGNATURE_MUTATION = gql`
+  mutation CreateSignatureMutation {
+    createImageSignature {
+      signature
+      timestamp
+    }
+  }
+`;
+
+interface IUploadImageResponse {
+  secure_url: string;
+}
+
+async function uploadImage(
+  image: File,
+  signature: string,
+  timestamp: number
+): Promise<IUploadImageResponse> {
+  const url = `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/upload`;
+
+  const formData = new FormData();
+
+  formData.append("file", image);
+  formData.append("signature", signature);
+  formData.append("timestamp", timestamp.toString());
+  formData.append("api_key", process.env.NEXT_PUBLIC_CLOUDINARY_KEY ?? "");
+
+  const response = await fetch(url, {
+    method: "post",
+    body: formData,
+  });
+
+  return response.json();
+}
 
 interface IFormData {
   address: string;
@@ -33,6 +67,8 @@ export default function HouseForm({}: IProps) {
     useForm<IFormData>({ defaultValues: {} });
 
   const address = watch("address");
+  const [createSignature] =
+    useMutation<CreateSignatureMutation>(SIGNATURE_MUTATION);
 
   useEffect(() => {
     register({ name: "address" }, { required: "Please enter your address" }),
@@ -41,7 +77,11 @@ export default function HouseForm({}: IProps) {
   }, [register]);
 
   const handleCreate = async (data: IFormData) => {
-    console.log({data})
+    const { data: signatureData } = await createSignature();
+    if (signatureData) {
+      const { signature, timestamp } = signatureData.createImageSignature;
+      const imageData = await uploadImage(data.image[0], signature, timestamp);
+    }
   };
 
   const onSubmit = (data: IFormData) => {
@@ -67,76 +107,78 @@ export default function HouseForm({}: IProps) {
         />
         {errors.address && <p>{errors.address.message}</p>}
       </div>
-      {address && <>
-        {" "}
-        <div className="mt-4">
-          <label
-            htmlFor="image"
-            className="p-4 border-dashed border-4 border-gray-600 block cursor-pointer"
-          >
-            {" "}
-            Click to add image(16:9)
-          </label>
-          <input
-            id="image"
-            name="image"
-            type="file"
-            accept="image/*"
-            style={{ display: "none" }}
-            ref={register({
-              validate: (fileList: FileList) => {
-                if (fileList.length === 1) return true;
-                return "Please upload one file";
-              },
-            })}
-            onChange={(event: ChangeEvent<HTMLInputElement>) => {
-              if (event?.target?.files?.[0]) {
-                const file = event.target.files[0];
-                const reader = new FileReader();
-                reader.onloadend = () => {
-                  setPreviewImage(reader.result as string);
-                };
-                reader.readAsDataURL(file);
-              }
-            }}
-          />
-          {previewImage && (
-            <img
-              src={previewImage}
-              className="m-t object-cover"
-              style={{ width: "576px", height: `${(9 / 16) * 576}px` }}
+      {address && (
+        <>
+          {" "}
+          <div className="mt-4">
+            <label
+              htmlFor="image"
+              className="p-4 border-dashed border-4 border-gray-600 block cursor-pointer"
+            >
+              {" "}
+              Click to add image(16:9)
+            </label>
+            <input
+              id="image"
+              name="image"
+              type="file"
+              accept="image/*"
+              style={{ display: "none" }}
+              ref={register({
+                validate: (fileList: FileList) => {
+                  if (fileList.length === 1) return true;
+                  return "Please upload one file";
+                },
+              })}
+              onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                if (event?.target?.files?.[0]) {
+                  const file = event.target.files[0];
+                  const reader = new FileReader();
+                  reader.onloadend = () => {
+                    setPreviewImage(reader.result as string);
+                  };
+                  reader.readAsDataURL(file);
+                }
+              }}
             />
-          )}
-          {errors.image && <p>{errors.image.message}</p>}
-        </div>
-        <div className="mt-4">
-          <label htmlFor="bedrooms" className="block">
-            Beds
-          </label>
-          <input
-            id="bedrooms"
-            name="bedrooms"
-            type="number"
-            className="p-2"
-            ref={register({
-              required: "Please enter the number of bedrooms",
-              max: { value: 10, message: "Wooahh, too big of a house" },
-              min: { value: 1, message: "Must have at least one bedroom" },
-            })}
-          />
-          {errors.bedrooms && <p>{errors.bedrooms.message}</p>}
-        </div>
-        <div className="mt-4">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded"
-            type="submit"
-            disabled={submitting}
-          >
-            Save
-          </button>{" "}
-          <Link href="/">Cancel</Link>
-        </div>
-      </>}
+            {previewImage && (
+              <img
+                src={previewImage}
+                className="m-t object-cover"
+                style={{ width: "576px", height: `${(9 / 16) * 576}px` }}
+              />
+            )}
+            {errors.image && <p>{errors.image.message}</p>}
+          </div>
+          <div className="mt-4">
+            <label htmlFor="bedrooms" className="block">
+              Beds
+            </label>
+            <input
+              id="bedrooms"
+              name="bedrooms"
+              type="number"
+              className="p-2"
+              ref={register({
+                required: "Please enter the number of bedrooms",
+                max: { value: 10, message: "Wooahh, too big of a house" },
+                min: { value: 1, message: "Must have at least one bedroom" },
+              })}
+            />
+            {errors.bedrooms && <p>{errors.bedrooms.message}</p>}
+          </div>
+          <div className="mt-4">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded"
+              type="submit"
+              disabled={submitting}
+            >
+              Save
+            </button>{" "}
+            <Link href="/">Cancel</Link>
+          </div>
+        </>
+      )}
     </form>
   );
 }
