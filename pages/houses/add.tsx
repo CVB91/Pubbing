80c802a42356a3204a4b1c3ceff3cf7dde6f1813
Christian <cphvb91@gmail.com>
2023-03-06 14:54:15 +0100

    houses add

--- pages/houses/add.tsx
+++ pages/houses/add.tsx
@@ -1,8 +1,26 @@
-// import { GetServerSideProps, NextApiRequest } from "next";
-// import { loadIdToken } from "src/auth/firebaseAdmin";
-// import Layout from "src/components/layout";
-// import HouseForm from "src/components/houseForm";
+import { GetServerSideProps, NextApiRequest } from "next";
+import { loadIdToken } from "src/auth/firebaseAdmin";
+import Layout from "src/components/layout";
+import HouseForm from "src/components/houseForm";
 
 export default function Add() {
-  return <div>Add</div>;
+  return <Layout main={<HouseForm/> } />
+}
+
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const uid = await loadIdToken(req as NextApiRequest)
+  
+
+//this function will redirect the user to the Auth page if they are not already logged in
+
+  if (!uid) {
+    res.setHeader("location", "/auth")
+    res.statusCode = 302
+    res.end()
+
+
+    
+  }
+  return { props: {} } 
 }
